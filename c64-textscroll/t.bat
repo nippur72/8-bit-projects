7b710fe1215506b0e917d7e4383f4557901c3549
nino-porcino <nino.porcino@gmail.com>
2019-02-23 15:30:58 +0100

    add c64-textscroll (without README)

---
+++ c64-textscroll/t.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set SOURCE=textscroll
+
+call asmproc -i %SOURCE%.lm -o %SOURCE%.asm -t dasm 
+if %errorlevel% == -1 goto fine
+dasm %SOURCE%.asm -f1 -l%SOURCE%.lst -o%SOURCE%.prg
+if %errorlevel% == -1 goto fine
+dir %SOURCE%.prg 
+:fine
