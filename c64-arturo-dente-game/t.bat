9e4451f2b47af488c74d494ecc01caf1f00bf0ae
nino-porcino <nino.porcino@gmail.com>
2019-02-24 11:04:39 +0100

    game by Arturo Dente

---
+++ c64-arturo-dente-game/t.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set SOURCE=game
+
+rem call asmproc -i %SOURCE%.lm -o %SOURCE%.asm -t dasm 
+node ..\..\asmproc\asmproc -i %SOURCE%.lm -o %SOURCE%.asm -t dasm 
+if %errorlevel% == -1 goto fine
+dasm %SOURCE%.asm -f1 -l%SOURCE%.lst -o%SOURCE%.prg
+if %errorlevel% == -1 goto fine
+dir %SOURCE%.prg 
+:fine
