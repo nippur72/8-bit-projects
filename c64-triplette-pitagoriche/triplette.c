49353910c388d06fcfdd4d9b3e8d87203bea53f4
nino-porcino <nino.porcino@gmail.com>
2019-06-09 17:46:29 +0200

    make it faster

--- c64-triplette-pitagoriche/triplette.c
+++ c64-triplette-pitagoriche/triplette.c
@@ -21,32 +21,92 @@ clock_t elapsed;
 // variabili per velocizzare il calcolo matriciale
 unsigned int a,b,c;
 unsigned int a2,b2,c2,c3;
+unsigned int k;
 
 // genera la tripletta tp * M1 e la accoda in buf_write
-void mmul1() {    
-   buf_write[buf_write_length] = a  + -b2 + c2;   ++buf_write_length;
-   buf_write[buf_write_length] = a2 + -b  + c2;   ++buf_write_length;
-   buf_write[buf_write_length] = a2 + -b2 + c3;   ++buf_write_length;
+void mmul1() { 
+   k = a  -b2 + c2; wptr[0] = k; 
+   k = a2 -b  + c2; wptr[1] = k; 
+   k = a2 -b2 + c3; wptr[2] = k; wptr += 3;   
+
+   /*       
+   k = a  -b2 + c2; *wptr = k; ++wptr;
+   k = a2 -b  + c2; *wptr = k; ++wptr;
+   k = a2 -b2 + c3; *wptr = k; ++wptr;   
+   */
+
+   /*
+   *wptr = a  -b2 + c2;   ++wptr;
+   *wptr = a2 -b  + c2;   ++wptr;
+   *wptr = a2 -b2 + c3;   ++wptr;
+   buf_write_length += 3;
+   */
+
+   /*
+   buf_write[buf_write_length] = a  -b2 + c2;   ++buf_write_length;
+   buf_write[buf_write_length] = a2 -b  + c2;   ++buf_write_length;
+   buf_write[buf_write_length] = a2 -b2 + c3;   ++buf_write_length;
+   */
 }
 
 // genera la tripletta tp * M2 e la accoda in buf_write
 void mmul2() {   
+   k = a  + b2 + c2; wptr[0] = k;  
+   k = a2 + b  + c2; wptr[1] = k;  
+   k = a2 + b2 + c3; wptr[2] = k;  wptr+=3;  
+
+   /*
+   k = a  + b2 + c2; *wptr = k;  ++wptr;
+   k = a2 + b  + c2; *wptr = k;  ++wptr;
+   k = a2 + b2 + c3; *wptr = k;  ++wptr;  
+   */
+
+   /*
+   *wptr = a  + b2 + c2;    ++wptr;
+   *wptr = a2 + b  + c2;    ++wptr;
+   *wptr = a2 + b2 + c3;    ++wptr;
+   buf_write_length += 3;
+   */
+
+   /*
    buf_write[buf_write_length] = a  + b2 + c2;    ++buf_write_length;
    buf_write[buf_write_length] = a2 + b  + c2;    ++buf_write_length;
    buf_write[buf_write_length] = a2 + b2 + c3;    ++buf_write_length;
+   */
 }
 
 // genera la tripletta tp * M3 e la accoda in buf_write
 void mmul3() {   
-   buf_write[buf_write_length] = -a  + b2 + c2;   ++buf_write_length;
-   buf_write[buf_write_length] = -a2 + b  + c2;   ++buf_write_length;
-   buf_write[buf_write_length] = -a2 + b2 + c3;   ++buf_write_length;
+   k = b2 -a  + c2; wptr[0] = k;  
+   k = b  -a2 + c2; wptr[1] = k;  
+   k = b2 -a2 + c3; wptr[2] = k;  wptr += 3;
+
+   /*
+   k = b2 -a  + c2; *wptr = k;  ++wptr;
+   k = b  -a2 + c2; *wptr = k;  ++wptr;
+   k = b2 -a2 + c3; *wptr = k;  ++wptr;
+   */
+
+   /*
+   *wptr = b2 -a  + c2;   ++wptr;
+   *wptr = b  -a2 + c2;   ++wptr;
+   *wptr = b2 -a2 + c3;   ++wptr;
+   buf_write_length += 3;
+   */
+
+   /*
+   buf_write[buf_write_length] = b2 -a  + c2;   ++buf_write_length;
+   buf_write[buf_write_length] = b  -a2 + c2;   ++buf_write_length;
+   buf_write[buf_write_length] = b2 -a2 + c3;   ++buf_write_length;
+   */
 }
 
 // funzione di stampa della tripletta appena generata nel buffer buf_write
 void stampa() {
-   tmp = &buf_write[buf_write_length-3];
-   printf("%4u:%5u,%5u,%5u \n", n, tmp[0], tmp[1], tmp[2]);
+   //tmp = &buf_write[buf_write_length-3];
+   tmp = wptr - 3;
+   //printf("%4u:%5u,%5u,%5u \n", n, tmp[0], tmp[1], tmp[2]);
+   printf("%5u,%5u,%5u \n", tmp[0], tmp[1], tmp[2]);
 }
 
 void main() {   
@@ -70,15 +130,17 @@ void main() {
    while(n<=1000)
    {
       buf_write_length = 0;   // resetta il buffer di scrittura
+      wptr = buf_write;       // wptr punta al buffer di scrittura
+      tp = buf_read;
 
-      for(i=0; i<buf_read_length; i=i+3) {
-
-         tp = &buf_read[i];  // legge tripletta
-
+      for(i=0; i<buf_read_length; i+=3) {
+         
+         //tp = &buf_read[i];  // legge tripletta
          a = tp[0]; // legge i numeri dalla tripletta per velocizzare il calcolo
          b = tp[1]; // legge i numeri dalla tripletta per velocizzare il calcolo
-         c = tp[2]; // legge i numeri dalla tripletta per velocizzare il calcolo
-         
+         c = tp[2]; // legge i numeri dalla tripletta per velocizzare il calcolo         
+         tp += 3;
+
          a2 = a*2; // precalcola elementi per velocizzare
          b2 = b*2; // precalcola elementi per velocizzare
          c2 = c*2; // precalcola elementi per velocizzare
@@ -91,10 +153,11 @@ void main() {
       }   
 
       // scambia i buffer di scrittura e lettura
+      buf_read_length = wptr - buf_write /*buf_write_length*/;      
+
       tmp = buf_read;
       buf_read = buf_write;
       buf_write = tmp;      
-      buf_read_length = buf_write_length;      
    }
 
    elapsed = (clock()-start);
