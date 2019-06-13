925d7ceb21f2d8f6d8fbe9195402decd9aae7c35
nino-porcino <nino.porcino@gmail.com>
2019-06-13 10:17:39 +0200

    use 16/32 bits; improve timer

--- c64-triplette-pitagoriche/triplette.c
+++ c64-triplette-pitagoriche/triplette.c
@@ -2,26 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef unsigned int ttrip;
+
 unsigned int n = 1;  // numero di triplette generate
 
-unsigned int *buf_read;    // buffer con le triplette da esaminare
-unsigned int *buf_write;   // buffer dove scrivere le triplette generate
+ttrip buf1[1700 * 3];   // buffer1
+ttrip buf2[1700 * 3];   // buffer2
+ttrip buf_calc[50 * 3]; // buffer dove scrivere le triplette calcolate nel range 981-1000
+
+ttrip *buf_read;    // puntatore al buffer con le triplette da esaminare
+ttrip *buf_write;   // puntatore al buffer dove scrivere le triplette generate
 
 unsigned int buf_read_length;   // dimensione del buffer di lettura
 unsigned int buf_write_length;  // dimensione del buffer di scrittura
+unsigned int buf_calc_length;   // dimensione del buffer di calcolo
 
-unsigned int i;      // variabile per ciclare sopre le triplette
-unsigned int *tp;    // tripletta corrente
-unsigned int *tmp;   // variabile temporanea usata per swappare i buffer
-unsigned int *wptr;  // variabile temporanea usata per swappare i buffer
+unsigned int i;   // variabile per ciclare sopre le triplette
+
+ttrip *tp;        // tripletta corrente
+ttrip *tmp,*tmp1; // variabile temporanea usata per swappare i buffer
+ttrip *wptr;      // variabile temporanea usata per swappare i buffer
 
 clock_t start;
 clock_t elapsed;
 
 // variabili per velocizzare il calcolo matriciale
-unsigned int a,b,c;
-unsigned int a2,b2,c2,c3;
-unsigned int k;
+ttrip a,b,c;
+ttrip a2,b2,c2,c3;
+ttrip k;
 
 // genera la tripletta tp * M1 e la accoda in buf_write
 void mmul1() { 
@@ -101,12 +109,30 @@ void mmul3() {
    */
 }
 
+// salva la tripletta appena generata nel buffer buf_calc
+void salva() {   
+   tmp  = wptr - 3;
+   tmp1 = &buf_calc[buf_calc_length];
+
+   tmp1[0] = tmp[0];
+   tmp1[1] = tmp[1];
+   tmp1[2] = tmp[2];
+
+   buf_calc_length += 3;   
+}
+
 // funzione di stampa della tripletta appena generata nel buffer buf_write
 void stampa() {
-   //tmp = &buf_write[buf_write_length-3];
-   tmp = wptr - 3;
-   //printf("%4u:%5u,%5u,%5u \n", n, tmp[0], tmp[1], tmp[2]);
-   printf("%5u,%5u,%5u \n", tmp[0], tmp[1], tmp[2]);
+   for(i=0;i<buf_calc_length;i+=3)
+   {   
+      tmp = &buf_calc[i];   
+      /*
+      if(sizeof(ttrip)==4) printf("%4u:%5lu,%5lu,%5lu \n", n, tmp[0], tmp[1], tmp[2]);
+      else                 printf("%4u:%5u,%5u,%5u \n"   , n, tmp[0], tmp[1], tmp[2]);
+      */
+      if(sizeof(ttrip)==4) printf("%4u:%5lu,%5lu,%5lu \n", i, tmp[0], tmp[1], tmp[2]);
+      else                 printf("%4u:%5u,%5u,%5u \n", 981+i, tmp[0], tmp[1], tmp[2]);
+   }      
 }
 
 void main() {   
@@ -118,14 +144,16 @@ void main() {
    // tiene traccia del tempo di inizio per misurare quanto impiega
    start = clock();
 
-   buf_read  = malloc(2000 * 3 * 2);  // crea buffer di lettura per almeno 2000 triplette
-   buf_write = malloc(2000 * 3 * 2);  // crea buffer di scrittura per almeno 2000 triplette
+   buf_read  = buf1;  // inizializza buffer di lettura
+   buf_write = buf2;  // inizializza buffer di scrittura
    
    // scrive tripletta iniziale 3,4,5
    buf_read[0] = 3;
    buf_read[1] = 4;
    buf_read[2] = 5;
+
    buf_read_length = 3;
+   buf_calc_length = 0;
 
    while(n<=1000)
    {
@@ -146,10 +174,10 @@ void main() {
          c2 = c*2; // precalcola elementi per velocizzare
          c3 = c*3; // precalcola elementi per velocizzare
 
-         mmul1(); ++n; if(n>=981 && n<=1000) stampa();
-         mmul2(); ++n; if(n>=981 && n<=1000) stampa();
-         mmul3(); ++n; if(n>=981 && n<=1000) stampa();
-         if(n>=1000) break;      
+         mmul1(); ++n; if(n>=981 && n<=1000) salva();
+         mmul2(); ++n; if(n>=981 && n<=1000) salva();
+         mmul3(); ++n; if(n>=981 && n<=1000) salva();
+         if(n>=1000) break;           
       }   
 
       // scambia i buffer di scrittura e lettura
@@ -161,7 +189,10 @@ void main() {
    }
 
    elapsed = (clock()-start);
-   printf("\nelapsed: %lu ticks (%lu secs)", elapsed, elapsed / CLOCKS_PER_SEC);
+
+   stampa();
+
+   printf("\nelapsed: %lu ticks (%lu msecs)", elapsed, (elapsed * 1000) / CLOCKS_PER_SEC);
    getchar();
 }
 
