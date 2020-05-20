4d29e7c2f2fddbcf7a67ccb3dd1dc78d0c919ee5
nino-porcino <nino.porcino@gmail.com>
2020-05-20 17:20:55 +0200

    Update m.bat

--- c64-piano/m.bat
+++ c64-piano/m.bat
@@ -1,6 +1,7 @@
 @echo off
 rem node freq.js > freq.lm
 set name=piano
+rem call asmproc -i %name%.lm -o %name%.asm -d KBD_SIEL
 call asmproc -i %name%.lm -o %name%.asm
 rem call asmproc -i %name%.lm -o %name%.asm -d DEBUG
 dasm %name%.asm -o%name%.prg -l%name%.lst
