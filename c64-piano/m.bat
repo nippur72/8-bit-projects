b13bac98b297f6e981daad248676475cd5d927d2
nino-porcino <nino.porcino@gmail.com>
2020-05-22 09:28:00 +0200

    move c64-piano to a separate repo

--- c64-piano/m.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem node freq.js > freq.lm
-set name=piano
-rem call asmproc -i %name%.lm -o %name%.asm -d KBD_SIEL
-call asmproc -i %name%.lm -o %name%.asm
-rem call asmproc -i %name%.lm -o %name%.asm -d DEBUG
-dasm %name%.asm -o%name%.prg -l%name%.lst
-
-
